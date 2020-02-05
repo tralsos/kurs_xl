511516601791017fe75d681a5977078582269b81
Trine Alsos <tral@statoil.com>
2020-02-05 13:23:17 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name = 'xl',
     packages = ['xl'],
-    version = "0.0.1",
+    version = "0.0.2",
     requirements = ['pandas','xlrd'],
     entry_points = {"console_scripts" : ["xl=xl:main"]},
 
